0fec135fb817621df710ab38432e3267804b76f0
Bioparco <monnimarco90@gmail.com>
2023-07-20 23:44:48 +0200

    Tables creation handler complete

--- src/db/db-utility.ts
+++ src/db/db-utility.ts
@@ -37,6 +37,8 @@ const checkIfTableExists = async (tableName: string): Promise<boolean> => {
   const client = await pool.connect();
   try {
     const userTableCheck = await checkIfTableExists('users');
+    const wishlistsTableCheck = await checkIfTableExists('wishlists');
+    const wishlistsItemsTableCheck = await checkIfTableExists('wishlistsItems');
 
     if (!userTableCheck) {
       const userQuery = `CREATE TABLE Users (
@@ -47,11 +49,37 @@ const checkIfTableExists = async (tableName: string): Promise<boolean> => {
       );`;
       await client.query(userQuery);
     }
+
+    if (!wishlistsTableCheck) {
+      const wishlistsQuery = `CREATE TABLE Wishlists (
+        wishlist_id UUID PRIMARY KEY DEFAULT '${uuidv4()}',
+        user_id UUID NOT NULL REFERENCES Users(user_id),
+        name VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+      );`;
+      await client.query(wishlistsQuery);
+    }
+
+    if (!wishlistsItemsTableCheck) {
+      const itemsQuery = `CREATE TABLE WishlistItems (
+        item_id UUID PRIMARY KEY DEFAULT '${uuidv4()}',
+        wishlist_id UUID NOT NULL REFERENCES Wishlists(wishlist_id),
+        name VARCHAR(255) NOT NULL,
+        description TEXT,
+        added_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        is_completed BOOLEAN DEFAULT FALSE
+      );`;
+      await client.query(itemsQuery);
+    }
   } catch (e) {
     console.log(e);
+  } finally {
+    client.release();
   }
 })();
 
+// Funzione 
 export const query = async (
   text: string,
   values?: any[]
