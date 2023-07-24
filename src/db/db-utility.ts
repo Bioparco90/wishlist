1a194494eb4d5af42e97e1aff1afe137fc1aed3d
Bioparco <monnimarco90@gmail.com>
2023-07-25 00:24:35 +0200

    Fixed database creation, there was a uuid problem

--- src/db/db-utility.ts
+++ src/db/db-utility.ts
@@ -1,6 +1,5 @@
 import Pool from 'pg-pool';
 import 'dotenv/config';
-import { v4 as uuidv4 } from 'uuid';
 import { QueryResult } from 'pg';
 
 const pool = new Pool({
@@ -42,7 +41,7 @@ const checkIfTableExists = async (tableName: string): Promise<boolean> => {
 
     if (!userTableCheck) {
       const userQuery = `CREATE TABLE Users (
-        user_id UUID DEFAULT '${uuidv4()}' PRIMARY KEY,
+        user_id UUID PRIMARY KEY,
         username VARCHAR(100) NOT NULL UNIQUE,
         email VARCHAR(255) UNIQUE,
         password VARCHAR(100) NOT NULL
@@ -52,7 +51,7 @@ const checkIfTableExists = async (tableName: string): Promise<boolean> => {
 
     if (!wishlistsTableCheck) {
       const wishlistsQuery = `CREATE TABLE Wishlists (
-        wishlist_id UUID PRIMARY KEY DEFAULT '${uuidv4()}',
+        wishlist_id UUID PRIMARY KEY',
         user_id UUID NOT NULL REFERENCES Users(user_id),
         name VARCHAR(255) NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -63,9 +62,9 @@ const checkIfTableExists = async (tableName: string): Promise<boolean> => {
 
     if (!itemsTableCheck) {
       const itemsQuery = `CREATE TABLE Items (
-        item_id UUID PRIMARY KEY DEFAULT '${uuidv4()}',
+        item_id UUID PRIMARY KEY',
         wishlist_id UUID NOT NULL REFERENCES Wishlists(wishlist_id),
-        name VARCHAR(255) NOT NULL,
+        item_name VARCHAR(255) NOT NULL,
         description TEXT,
         added_at TIMESTAMP NOT NULL DEFAULT NOW(),
         is_completed BOOLEAN DEFAULT FALSE
