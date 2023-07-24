ed4cae866f3ad0076ff78c68b78d3678a6e66f7e
Bioparco <monnimarco90@gmail.com>
2023-07-25 00:39:51 +0200

    user's wishlists getter route done

--- src/routes/wishlist.ts
+++ src/routes/wishlist.ts
@@ -7,15 +7,22 @@ const wishlist = Router();
 
 wishlist.get('/', authenticateToken, async (req, res) => {
   const { user_id } = req.body.userData;
-
-  res.json({ message: 'ok', user_id: user_id });
+  const queryString = 'SELECT * FROM wishlists WHERE user_id=$1';
+  const queryValues = [user_id];
+  try {
+    const { rows } = await query(queryString, queryValues);
+    res.json(rows);
+  } catch (e) {
+    res.json({ error_message: (e as Error).message });
+  }
 });
 
 wishlist.post('/', authenticateToken, async (req, res) => {
   const { user_id } = req.body.userData;
   const { name } = req.body;
   const wishlist_id = uuidv4();
-  const queryString = 'INSERT INTO wishlists(wishlist_id, user_id, name) VALUES ($1, $2, $3)';
+  const queryString =
+    'INSERT INTO wishlists(wishlist_id, user_id, name) VALUES ($1, $2, $3)';
   const queryValues = [wishlist_id, user_id, name];
   try {
     await query(queryString, queryValues);
